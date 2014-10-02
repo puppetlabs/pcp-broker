7cb9ea0c1ac76b086ef8959c9e5597c26b2d9c40
Richard Clamp <richardc@unixbeard.net>
2014-10-02 13:46:19 +0100

    Add partial testing to the hazelcast mesh

--- test/puppetlabs/cthun/meshing/hazelcast_test.clj
+++ test/puppetlabs/cthun/meshing/hazelcast_test.clj
@@ -1,3 +1,14 @@
 (ns puppetlabs.cthun.meshing.hazelcast-test
   (require [clojure.test :refer :all]
            [puppetlabs.cthun.meshing.hazelcast :refer :all]))
+
+(deftest broker-for-endpoint-test
+  (with-redefs [flatten-location-map (fn [_] [{:broker "bill" :endpoint "cth://localhost/agent"}
+                                              {:broker "ted"  :endpoint "cth://localhost/agent"}
+                                              {:broker "bill" :endpoint "cth://localhost/controller"}])]
+    (testing "it finds the controller"
+      (is (= "bill" (broker-for-endpoint nil "cth://localhost/controller"))))
+    (testing "it finds the agent"
+      (is (= "bill" (broker-for-endpoint nil "cth://localhost/agent"))))
+    (testing "it won't find something absent"
+      (is (not (broker-for-endpoint nil "cth://localhost/no-suchagent"))))))
