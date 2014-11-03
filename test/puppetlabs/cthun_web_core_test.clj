282bba97d5f11b1582bf091f7f068dacc7af3d55
Richard Clamp <richardc@unixbeard.net>
2014-11-03 20:25:56 +0000

    maint - remove tests for webserver-service
    Completes a254836

--- test/puppetlabs/cthun_web_core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns puppetlabs.cthun-web-core-test
-  (:require [clojure.test :refer :all]
-            [puppetlabs.cthun-service :as hello-svc]
-            [puppetlabs.cthun-web-core :refer :all]
-            [ring.mock.request :as mock]))
-
-(deftest cthun-web-test
-  (testing "Not yet implemented"
-    (is (= 1 1))))
