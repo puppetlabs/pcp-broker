0994df454cb71a06842174237bc21406afc05fc4
Jenkins CI <ci@puppetlabs.com>
2015-10-15 10:08:00 -0700

    Version 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/pcp-broker "0.4.0"
+(defproject puppetlabs/pcp-broker "0.4.1-SNAPSHOT"
   :description "PCP fabric messaging broker"
   :url "https://github.com/puppetlabs/pcp-broker"
   :license {:name "Apache License, Version 2.0"
