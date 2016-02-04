839a77204ca043d25b5b3ae7f21c7cd42dc83a8e
Richard Clamp <richardc@unixbeard.net>
2016-02-04 16:04:17 +0000

    (maint) Rework dotestseq macro to be more useful
    The original version of the dotestseq macro from puppetdb didn't
    show the values of the bindings per iteration in the test name,
    only the names of them.
    
        Testing case (version)
        Testing case (version)
        Testing case (sender-version receiver-version)
        Testing case (sender-version receiver-version)
    
    This rework from Michal Růžička enhances this so we get better annotated
    test names like so:
    
        Testing case: version: v1.0
        Testing case: version: vNext
        Testing case: sender-version: v1.0, receiver-version: v1.0
        Testing case: sender-version: v1.0, receiver-version: vNext

--- test/utils/puppetlabs/pcp/testutils.clj
+++ test/utils/puppetlabs/pcp/testutils.clj
@@ -6,5 +6,10 @@
     `(do ~@body)
     (let [case-versions (remove keyword? (take-nth 2 bindings))]
       `(doseq ~bindings
-         (testing (str "Testing case " '~case-versions)
+         (testing (str "Testing case: " (clojure.string/join
+                                         ", "
+                                         (map #(clojure.string/join ": " %)
+                                              (partition
+                                               2
+                                               (interleave '~case-versions (list ~@case-versions))))))
            ~@body)))))
