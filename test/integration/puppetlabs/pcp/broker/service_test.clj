c6996d6a018ecf64e4da3dc0228688943fabe1ce
Michal Růžička <michal.ruza@gmail.com>
2017-01-13 17:52:08 +0100

    (PCP-600) Inventory updates tests
    This restructures the test code to reflect the restructure of the
    tested code.
    New tests are added to test the inventory updates functionality.

--- test/integration/puppetlabs/pcp/broker/service_test.clj
+++ test/integration/puppetlabs/pcp/broker/service_test.clj
@@ -370,6 +370,100 @@
                    (is (= "http://puppetlabs.com/inventory_response" (:message_type response)))
                    (is (= [] (:uris data))))))))
 
+(deftest inventory-node-recieves-updates-when-inventory-changes-when-subscribed-to-updates
+  (with-app-with-config app broker-services broker-config
+    (dotestseq [version protocol-versions]
+               (with-open [client (client/connect :certname "client01.example.com"
+                                                  :version version)]
+                 (let [request (client/make-message
+                                 version
+                                 {:message_type "http://puppetlabs.com/inventory_request"
+                                  :target "pcp:///server"
+                                  :sender "pcp://client01.example.com/agent"
+                                  :data {:query ["pcp://client02.example.com/agent"]
+                                         :subscribe true}})]
+                   (client/send! client request))
+                 (let [response (client/recv! client)
+                       data (client/get-data response version)]
+                   (is (= "http://puppetlabs.com/inventory_response" (:message_type response)))
+                   (is (= [] (:uris data))))
+                 (with-open [client2 (client/connect :certname "client02.example.com"
+                                                    :version version)]
+                   (let [response (client/recv! client)
+                         data (client/get-data response version)]
+                     (is (= "http://puppetlabs.com/inventory_update" (:message_type response)))
+                     (is (= [{:client "pcp://client02.example.com/agent" :change 1}] (:changes data)))))
+                 (let [response (client/recv! client)
+                       data (client/get-data response version)]
+                   (is (= "http://puppetlabs.com/inventory_update" (:message_type response)))
+                   (is (= [{:client "pcp://client02.example.com/agent" :change -1}] (:changes data))))))))
+
+(deftest inventory-updates-are-properly-filtered
+  (with-app-with-config app broker-services broker-config
+    (dotestseq [version protocol-versions]
+               (with-open [client1 (client/connect :certname "client01.example.com"
+                                                   :version version)
+                           client2 (client/connect :certname "client02.example.com"
+                                                   :version version)]
+                 ;; subscribe client1 for updates with a specific filter
+                 (let [request (client/make-message
+                                 version
+                                 {:message_type "http://puppetlabs.com/inventory_request"
+                                  :target "pcp:///server"
+                                  :sender "pcp://client01.example.com/agent"
+                                  :data {:query ["pcp://client04.example.com/*"]
+                                         :subscribe true}})]
+                   (client/send! client1 request))
+                 (let [response (client/recv! client1)
+                       data (client/get-data response version)]
+                   (is (= "http://puppetlabs.com/inventory_response" (:message_type response)))
+                   (is (= [] (:uris data))))
+
+                 ;; subscribe client2 for updates with a match all filter
+                 (let [request (client/make-message
+                                 version
+                                 {:message_type "http://puppetlabs.com/inventory_request"
+                                  :target "pcp:///server"
+                                  :sender "pcp://client02.example.com/agent"
+                                  :data {:query ["pcp://*/agent"]
+                                         :subscribe true}})]
+                   (client/send! client2 request))
+                 (let [response (client/recv! client2)
+                       data (client/get-data response version)]
+                   (is (= "http://puppetlabs.com/inventory_response" (:message_type response)))
+                   (is (= ["pcp://client01.example.com/agent" "pcp://client02.example.com/agent"] (:uris data))))
+
+                 ;; now connect a client matching only the filter client2 used for subscribing
+                 (with-open [client3 (client/connect :certname "client03.example.com"
+                                                     :version version)]
+                   (let [response (client/recv! client2)
+                         data (client/get-data response version)]
+                     (is (= "http://puppetlabs.com/inventory_update" (:message_type response)))
+                     (is (= [{:client "pcp://client03.example.com/agent" :change 1}] (:changes data))))
+                   ;; client1 doesn't receive an update at all, becuase the change doesn't match its filter
+                   (let [response (client/recv! client1 3000)]
+                     (is (nil? response)))
+                   (with-open [client4 (client/connect :certname "client04.example.com"
+                                                       :version version)]
+                     (let [response (client/recv! client2)
+                           data (client/get-data response version)]
+                       (is (= "http://puppetlabs.com/inventory_update" (:message_type response)))
+                       (is (= [{:client "pcp://client04.example.com/agent" :change 1}] (:changes data))))
+                     (let [response (client/recv! client1)
+                           data (client/get-data response version)]
+                       (is (= "http://puppetlabs.com/inventory_update" (:message_type response)))
+                       (is (= [{:client "pcp://client04.example.com/agent" :change 1}] (:changes data))))))
+                 ;; client4 & client3 have disconnected (in that order)
+                 (let [response (client/recv! client2)
+                       data (client/get-data response version)]
+                   (is (= "http://puppetlabs.com/inventory_update" (:message_type response)))
+                   (is (= [{:client "pcp://client04.example.com/agent" :change -1}
+                           {:client "pcp://client03.example.com/agent" :change -1}] (:changes data))))
+                 (let [response (client/recv! client1)
+                       data (client/get-data response version)]
+                   (is (= "http://puppetlabs.com/inventory_update" (:message_type response)))
+                   (is (= [{:client "pcp://client04.example.com/agent" :change -1}] (:changes data))))))))
+
 (def no-inventory-broker-config
   "A broker that allows connections but no inventory requests"
   (assoc-in broker-config [:authorization :rules]
--- test/unit/puppetlabs/pcp/broker/message_test.clj
+++ test/unit/puppetlabs/pcp/broker/message_test.clj
@@ -1,29 +1,29 @@
-(ns puppetlabs.pcp.broker.core-test
+(ns puppetlabs.pcp.broker.message-test
   (:require [clojure.test :refer :all]
-            [puppetlabs.pcp.broker.message :as message]))
+            [puppetlabs.pcp.broker.message :refer :all]))
 
 (deftest multicast-message?-test
   (testing "returns false if target specifies a single host with no wildcards"
-    (let [message (message/make-message
+    (let [message (make-message
                    {:target "pcp://example01.example.com/foo"})]
       (is (not (multicast-message? message)))))
   (testing "returns true if target includes wildcard hostname"
-    (let [message (message/make-message
+    (let [message (make-message
                    {:target "pcp://*/foo"})]
       (is (multicast-message? message))))
   (testing "returns true if target includes wildcard client type"
-    (let [message (message/make-message
+    (let [message (make-message
                    {:target "pcp://example01.example.com/*"})]
       (is (multicast-message? message))))
   (testing "returns true if multicast-message key is inserted"
-    (let [message (message/make-message
-                   {:target "pcp://example01.example.com/agent"
-                    :multicast-message true})]
+    (let [message (-> (make-message
+                       {:target "pcp://example01.example.com/agent"})
+                      (assoc :multicast-message true))]
       (is (multicast-message? message)))))
 
 (deftest message-roundtrip-test
   (testing "message survives v1 roundtrip"
-    (let [message (message/make-message
+    (let [message (make-message
                    {:sender "pcp://gangoffour/entity"
                     :message_type "ether"
                     :target "pcp://gangoffour/entity"})]
@@ -31,7 +31,7 @@
                          v1-encode
                          v1-decode)))))
   (testing "message survives v2 roundtrip"
-    (let [message (message/make-message
+    (let [message (make-message
                    {:sender "pcp://gangoffour/entity"
                     :message_type "ether"
                     :target "pcp://gangoffour/entity"})]
